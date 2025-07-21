4b118ed730920d129cc2cd00c7ed03cacc040849
MALAMNES <ss10092002@mail.ru>
2025-07-21 20:59:25 +0300

    Code changed

--- cmd/main.go
+++ cmd/main.go
@@ -5,31 +5,43 @@ import (
 	"math"
 )
 
-func f(x float64) float64 {
-	return math.Pow(x, 3) + 8*math.Pow(x, 2) - 3*x + 3
-}
+func goldenSection(f func(float64) float64, a, b, eps float64) (int, float64) {
 
-func main() {
-	var a, b float64 = 5, 5.00001
-	var x1, x2 float64
-	nIter := 1
-	eps := 1e-6
 	phi := (1 + math.Sqrt(5)) / 2 // 1.6188...
+	var xMin float64
+	nIter := 0
 
-	for math.Abs(b-a) > eps {
+	var recurse func(a, b float64)
+	recurse = func(a, b float64) {
 		nIter++
 
-		x1 = b - (b-a)/phi
-		x2 = a + (b-a)/phi
+		if math.Abs(b-a) < eps {
+			xMin = (a + math.Sqrt(b)) / 2
+			return
+		}
+
+		x1 := b - (b-a)/phi
+		x2 := a + (b-a)/phi
 
 		if f(x1) < f(x2) {
-			b = x2
+			recurse(a, x2)
 		} else {
-			a = x1
+			recurse(x1, b)
 		}
 	}
 
-	xmin := (a + math.Sqrt(b)) / 2
-	fmt.Printf("Iteretions: %d\nXmin = %.6f", nIter, xmin)
+	recurse(a, b)
+
+	return nIter, xMin
+}
+
+func main() {
+	f := func(x float64) float64 { return math.Pow(x, 3) + 8*math.Pow(x, 2) - 3*x + 3 }
+
+	var a, b float64 = 5, 5.00001
+	eps := 1e-6
+
+	nIter, xMin := goldenSection(f, a, b, eps)
+	fmt.Printf("Iteretions: %d\nXmin = %.6f", nIter, xMin)
 
 }
